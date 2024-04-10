60405364579aa09e841bb1d778c8c18a4a9a792e
zolcano <87764175+zolcano@users.noreply.github.com>
2024-04-10 16:58:29 +0200

    Delete myfirstapp directory

--- myfirstapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'myfirstapp/index.html')
-
-def formulaire(request):
-    return render(request, 'myfirstapp/formulaire.html')

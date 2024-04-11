edbd3872a0261ebc1d4ba78894a713f3c1fdc96c
zolcano <87764175+zolcano@users.noreply.github.com>
2024-04-11 11:47:10 +0200

    Delete R2.09 directory

--- R2.09/firstproject/bibliotheque/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ajout/', views.ajout),
-    path('traitement', views.traitement),
-]
--- R2.09/firstproject/myfirstapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index),
-    path('formulaire/', views.formulaire),
-    path('controleur/', views.controleur)
-]
